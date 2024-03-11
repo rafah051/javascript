0062d734963e0c63b498ad6cbf10efb34504b495
rafah <136349907+rafah051@users.noreply.github.com>
2024-03-11 09:10:03 -0300

    Add files via upload

---
+++ rafa.js
@@ -0,0 +1,12 @@
+// Variáveis representando idade e cidadania
+let idade = 25;
+let cidadania = true;
+
+// Verificar se a idade é maior
+
+if (idade >= 18 && cidadania == true ) { console.log ('Pode votar');
+
+}
+else {
+    console.log( 'Não pode votar');
+}
