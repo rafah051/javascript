606c61150c3d66ad95ec29aac069e7acea7a048f
rafah <136349907+rafah051@users.noreply.github.com>
2024-06-03 14:26:44 -0300

    Add files via upload

---
+++ Aula01/alterar.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    Nome: 'Henrique',
+    profissão: 'Engenheiro'
+};
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = '43 988727443'
+
+console.log(pessoa.telefone);
---
+++ Aula01/objeto.js
@@ -0,0 +1,17 @@
+//Guardando informações pessoais de clientes
+
+const informaçõesPessoal = ['Nome', 'Rafael', 'Idade', 17, 'CPF', '123.456.789-01']
+
+console.log(informaçõesPessoal)
+
+//Como fazer isso usando objetos?
+
+const objetoPessoal = {
+    Nome: 'Rafael',
+    Idade: 17,
+    CPF: '123.456.789-01',
+    Email: 'rafael@gmail.com',
+
+}
+
+console.log(`O nome do cliente é ${informaçõesPessoal.Nome}`)
